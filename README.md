590c86654d31f759f7c42ab34fb9b2057c48f50f
cyice <1207201395@qq.com>
2020-04-07 11:24:43 +0800

    添加README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# 毕业设计
+
+
+## 参考链接
+
+### SpringBoot jpa 关系注解
+> [https://www.jianshu.com/p/fc8c82671632](https://www.jianshu.com/p/fc8c82671632)
+
+### Spring Boot 中使用@DateTimeFormat和@JsonFormat注解
+> [https://blog.csdn.net/u011263794/article/details/84985185](https://blog.csdn.net/u011263794/article/details/84985185)
--- src/main/java/com/bookrecommend/demo/entity/Book.java
+++ src/main/java/com/bookrecommend/demo/entity/Book.java
@@ -1,10 +1,28 @@
 package com.bookrecommend.demo.entity;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 
 // 书籍表
 @Entity
+@Table(name = "book")
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "name_cn",nullable = false,length = 31)
+    private String nameCn;
+
+    @Column(name = "name_eng",length = 31)
+    private String nameEng;
+
+    @Column(name = "photo",nullable = false,length = 255)
+    private String photo;
+
+
+    private String authorId;
+
 }
