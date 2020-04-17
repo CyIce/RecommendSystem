38ec60b6ba662ef6aebc9abc72ce9c37fab4e180
cyice <1207201395@qq.com>
2020-04-17 21:05:51 +0800

    使用thymeleaf+bootstrap制作首页。

--- src/main/java/com/bookrecommend/demo/controller/Hello.java
+++ src/main/java/com/bookrecommend/demo/controller/Hello.java
@@ -1,14 +1,30 @@
 package com.bookrecommend.demo.controller;
 
-
+import com.bookrecommend.demo.entity.User;
+import com.bookrecommend.demo.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Hello {
 
-    @GetMapping(value = "/hello")
-    public String hello(){
-        return "Hello World!";
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+
+        User user = userRepository.getOne(1);
+
+        model.addAttribute("name", "thymeleaf");
+        model.addAttribute("user", user);
+        return "hello";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        return "index";
     }
 }
